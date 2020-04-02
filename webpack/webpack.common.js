00dfff3776ad52d8b9ddab04126d947c30dd33b3
kunmi_i <kunmii@khws.co.uk>
2020-04-02 01:15:55 +0100

    Added performance optimizations for prod build

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -36,6 +37,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../', 'src/index.html'),
     }),
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -18,9 +19,48 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name].[contentHash].css',
     }),
   ],
+  optimization: {
+    minimizer: [
+      new TerserWebpackPlugin({
+        terserOptions: {
+          compress: {
+            comparisons: false,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            comments: false,
+            ascii_only: true,
+          },
+          warnings: false,
+        },
+        // Use multi-process parallel running to improve build speed
+        parallel: true,
+        // Enable file caching
+        cache: true,
+      }),
+      new OptimizeCssAssetsPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+        },
+        common: {
+          minChunks: 2,
+          priority: -10,
+        },
+      },
+    },
+    // Keep the webpack runtime chunk separated to enable long term caching
+    runtimeChunk: 'single',
+  },
 };
